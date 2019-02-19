3aefb632e906d1bff590bf2141d950eceda7591c
Mark J. Olah <markjolah@users.noreply.github.com>
2019-02-19 12:23:08 -0700

    Armadillo INT64/INT32 support

---
+++ build.debug.int32.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# build.debug.sh <cmake args ...>
+# Debug-only build to local install prefix with build-tree export
+
+INSTALL_PATH=_install
+BUILD_PATH=_build/Debug
+NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
+
+ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
+ARGS="${ARGS} -DBUILD_TESTING=On"
+ARGS="${ARGS} -DBUILD_STATIC_LIBS=ON"
+ARGS="${ARGS} -DBUILD_SHARED_LIBS=ON"
+ARGS="${ARGS} -DBUILD_TESTING=On"
+ARGS="${ARGS} -DOPT_INSTALL_TESTING=On"
+ARGS="${ARGS} -DOPT_EXPORT_BUILD_TREE=On"
+ARGS="${ARGS} -DOPT_ARMADILLO_INT64=Off"
+
+set -ex
+rm -rf $BUILD_PATH
+cmake -H. -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Debug -Wdev ${ARGS} $@
+VERBOSE=1 cmake --build $BUILD_PATH --target all -- -j${NUM_PROCS}
